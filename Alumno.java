536ee524251890cd839e34af1ac94e55c800e0bc
guisnzmtz <dh.inf.eso.guillermosanchezm@gmail.com>
2025-04-05 18:57:54 +0200

    Añadimos el atributo edad y corregimos errores en la clase Alumno.java

--- Alumno.java
+++ Alumno.java
@@ -7,17 +7,19 @@ public class Alumno {
 	public List<Modulo> listaModulos;
 	public Vehiculo vehiculo;
 	private Grupo grupo;
-	private String apodo
+	private String apodo;
+	private int edad;
 
 
-	public Alumno(String dni, String numExpediente, String nombre, String apellido, Grupo grupo, String apodo ) {
+	public Alumno(String dni, String numExpediente, String nombre, String apellido, Grupo grupo, String apodo, int edad ) {
 		this.dni = dni;
 		this.numExpediente = numExpediente;
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.grupo = grupo;
 		this.listaModulos = new ArrayList<>();
-		this.apodo =apodo;
+		this.apodo = apodo;
+		this.edad = edad;
 	}
 	
 	public void matriculaModulo(Modulo modulo) {
